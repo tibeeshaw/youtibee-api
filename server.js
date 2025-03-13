fd17bb4239c2766ff63bd8e9c870b98173769d37
Tibeechaw <tibeechaw@gmail.com>
2025-03-13 23:46:41 +0100

    .

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.use(session({
     cookie: {
         secure: process.env.ENV === 'developpement' ? false : true, // Set to true in production with HTTPS
         httpOnly: true,
+        sameSite: "none", // Allow cross-origin cookies
+        domain: ".tibeechaw.com", // Set this if you have a custom domain
         maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days session expiration
     }
 }));
